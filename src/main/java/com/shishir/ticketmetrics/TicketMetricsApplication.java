029ff0ccb8a3e1fd8a2254f930c484c91dc73154
Shishir Keluskar <shishirkeluskar@users.noreply.github.com>
2025-07-10 22:30:06 +0300

    save-point: Create spring boot grpc project

---
+++ src/main/java/com/shishir/ticketmetrics/TicketMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.shishir.ticketmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketMetricsApplication.class, args);
+	}
+
+}
