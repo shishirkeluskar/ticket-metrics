285718bfd5f9a6eb5e6617568f50f61cdc1dc2dc
Shishir <shishirkeluskar@users.noreply.github.com>
2025-07-11 17:34:19 +0300

    Feature/project setup (#1)
    * save-point: Create spring boot grpc project
    
    * chor: Code style fix

---
+++ build.gradle.kts
@@ -0,0 +1,61 @@
+import com.google.protobuf.gradle.id
+
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.3"
+	id("io.spring.dependency-management") version "1.1.7"
+	id("com.google.protobuf") version "0.9.4"
+}
+
+group = "com.shishir.ticketmetrics"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+extra["springGrpcVersion"] = "0.9.0"
+
+dependencies {
+	implementation("io.grpc:grpc-services")
+	implementation("org.springframework.grpc:spring-grpc-spring-boot-starter")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.grpc:spring-grpc-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.grpc:spring-grpc-dependencies:${property("springGrpcVersion")}")
+	}
+}
+
+protobuf {
+	protoc {
+		artifact = "com.google.protobuf:protoc"
+	}
+	plugins {
+		id("grpc") {
+			artifact = "io.grpc:protoc-gen-grpc-java"
+		}
+	}
+	generateProtoTasks {
+		all().forEach {
+			it.plugins {
+				id("grpc") {
+					option("@generated=omit")
+				}
+			}
+		}
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/test/java/com/shishir/ticketmetrics/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.shishir.ticketmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+class ApplicationTests {
+  
+  @Test
+  void contextLoads() {
+    var list = List.of();
+  }
+}
