5ebaecf92047f018d42399fa9e5b67e6fa246b28
Shishir Keluskar <shishirkeluskar@users.noreply.github.com>
2025-07-11 17:33:10 +0300

    chor: Code style fix

--- src/main/java/com/shishir/ticketmetrics/Application.java
+++ src/main/java/com/shishir/ticketmetrics/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TicketMetricsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TicketMetricsApplication.class, args);
-	}
-
+public class Application {
+  
+  public static void main(String[] args) {
+    
+    SpringApplication.run(Application.class, args);
+  }
 }
--- src/test/java/com/shishir/ticketmetrics/ApplicationTests.java
+++ src/test/java/com/shishir/ticketmetrics/ApplicationTests.java
@@ -3,11 +3,13 @@ package com.shishir.ticketmetrics;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 @SpringBootTest
-class TicketMetricsApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+class ApplicationTests {
+  
+  @Test
+  void contextLoads() {
+    var list = List.of();
+  }
 }
