7a576d48579541da3f8117c8f3d755270cb7a934
Shishir Keluskar <shishirkeluskar@users.noreply.github.com>
2025-07-16 11:47:56 +0300

    save-point: Refactor.

--- src/main/java/com/shishir/ticketmetrics/grpc/TicketMetricsGrpcService.java
+++ src/main/java/com/shishir/ticketmetrics/grpc/TicketMetricsGrpcService.java
@@ -9,6 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.grpc.server.service.GrpcService;
 
+import java.util.function.Function;
+
 @GrpcService
 public class TicketMetricsGrpcService extends TicketMetricsServiceGrpc.TicketMetricsServiceImplBase {
   private static final Logger LOG = LoggerFactory.getLogger(TicketMetricsGrpcService.class);
@@ -21,64 +23,33 @@ public class TicketMetricsGrpcService extends TicketMetricsServiceGrpc.TicketMet
   
   @Override
   public void getTicketScore(GetTicketScoreRequest request, StreamObserver<GetTicketScoreResponse> responseObserver) {
-    try {
-      responseObserver.onNext(handler.handle(request));
-      responseObserver.onCompleted();
-    } catch (StatusRuntimeException e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(e);
-    } catch (Exception e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(Status.INTERNAL.withDescription("Internal error").withCause(e).asRuntimeException());
-    }
+    handleGrpcCall(request, handler::handle, responseObserver);
   }
   
   @Override
   public void getCategoryTimelineScores(CategoryTimelineRequest request, StreamObserver<CategoryTimelineResponse> responseObserver) {
-    try {
-      responseObserver.onNext(handler.handle(request));
-      responseObserver.onCompleted();
-    } catch (StatusRuntimeException e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(e);
-    } catch (Exception e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(Status.INTERNAL.withDescription("Internal error").withCause(e).asRuntimeException());
-    }
+    handleGrpcCall(request, handler::handle, responseObserver);
   }
   
   @Override
   public void getTicketCategoryScores(GetTicketCategoryScoresRequest request, StreamObserver<GetTicketCategoryScoresResponse> responseObserver) {
-    try {
-      responseObserver.onNext(handler.handle(request));
-      responseObserver.onCompleted();
-    } catch (StatusRuntimeException e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(e);
-    } catch (Exception e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(Status.INTERNAL.withDescription("Internal error").withCause(e).asRuntimeException());
-    }
+    handleGrpcCall(request, handler::handle, responseObserver);
   }
   
   @Override
   public void getOverallQualityScore(OverallQualityScoreRequest request, StreamObserver<OverallQualityScoreResponse> responseObserver) {
-    try {
-      responseObserver.onNext(handler.handle(request));
-      responseObserver.onCompleted();
-    } catch (StatusRuntimeException e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(e);
-    } catch (Exception e) {
-      LOG.error("Error encountered.", e);
-      responseObserver.onError(Status.INTERNAL.withDescription("Internal error").withCause(e).asRuntimeException());
-    }
+    handleGrpcCall(request, handler::handle, responseObserver);
   }
   
   @Override
   public void comparePeriodScores(PeriodScoreComparisonRequest request, StreamObserver<PeriodScoreComparisonResponse> responseObserver) {
+    handleGrpcCall(request, handler::handle, responseObserver);
+  }
+  
+  private <REQ, RESP> void handleGrpcCall(REQ request, Function<REQ, RESP> handlerFn, StreamObserver<RESP> responseObserver) {
     try {
-      responseObserver.onNext(handler.handle(request));
+      RESP response = handlerFn.apply(request);
+      responseObserver.onNext(response);
       responseObserver.onCompleted();
     } catch (StatusRuntimeException e) {
       LOG.error("Error encountered.", e);
